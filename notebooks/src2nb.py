e54fb453cfa9595da77ba585c8409d7e93c9ae88
Oscar Amarilla <o64.amarill@gmail.com>
2023-05-04 17:59:30 -0400

    Uploading the project.

---
+++ notebooks/src2nb.py
@@ -0,0 +1,11 @@
+"""This script exist just because I don't like the sys.path syntax
+in my notebook. Cheers!"""
+
+import sys
+
+import os
+
+# add the src directory to the Python path
+sys.path.append(os.path.abspath('../src'))
+
+
